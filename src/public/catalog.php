e6e94ab2f262bde8a177e0982299f07947a4a970
Matkarim Vahabov <matkarimvahabov@gmail.com>
2025-02-11 07:09:45 +0500

    создает сессию и проверяет

--- src/public/catalog.php
+++ src/public/catalog.php
@@ -1,19 +1,29 @@
 <?php
 
-if(!isset($_COOKIE['user_id'])) {
+session_start();
+
+//if(!isset($_COOKIE['user_id'])) {
+//    header("Location: /login_form.php");
+//}
+
+if(isset($_SESSION['userId'])){
+
+    $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
+    $stmt = $pdo->query('SELECT * FROM products');
+    $products = $stmt->fetchAll();
+
+//    echo '<pre>';
+//    print_r($products);
+//    echo '</pre>';
+
+    require_once './catalog_page.php';
+
+} else {
     header("Location: /login_form.php");
 }
 
-$pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
 
-$stmt = $pdo->query('SELECT * FROM products');
-$products = $stmt->fetchAll();
 
-//echo '<pre>';
-//print_r($products);
-//echo '</pre>';
-
-require_once './catalog_page.php';
 
 
 
