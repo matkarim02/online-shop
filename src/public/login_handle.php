4fe01b65cec412475580700f3c2e5747d22d1d98
Matkarim Vahabov <matkarimvahabov@gmail.com>
2025-02-11 07:08:25 +0500

    добавляет валидцию и создает сессию

--- src/public/login_handle.php
+++ src/public/login_handle.php
@@ -1,26 +1,62 @@
 <?php
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+function validate(array $data): array
+{
+    $errors = [];
 
-
-$pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
-$stmt = $pdo->prepare('SELECT * FROM users WHERE email = :email');
-$stmt->execute(['email' => $username]);
-$user = $stmt->fetch();
-
-$errors = [];
-
-if($user === false){
-    $errors['username'] = 'username or password is incorrect';
-} else {
-    $passwordDB = $user['password'];
-    if(password_verify($password, $passwordDB)){
-        setcookie('user_id', $user['id']);
-        header("Location: /catalog.php");
-    } else {
-        $errors['username'] = 'username or password is incorrect';
+    if(!isset($data['username'])){
+        $errors['username'] = "Username is required";
     }
+
+    if(!isset($data['password'])){
+        $errors['password'] = "Password is required";
+    }
+
+    return $errors;
 }
+
+$errors = validate($_POST);
+
+
+
+
+if(empty($errors)){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+
+    $pdo = new PDO('pgsql:host=postgres_db;port=5432;dbname=mydb', 'user', 'pass');
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = :email');
+    $stmt->execute(['email' => $username]);
+    $user = $stmt->fetch();
+
+    $errors = [];
+
+    if($user === false){
+        $errors['username'] = 'username or password is incorrect';
+    } else {
+        $passwordDB = $user['password'];
+        if(password_verify($password, $passwordDB)){
+
+            //успешный вход через сессии
+            session_start();
+            $_SESSION['userId'] = $user['id'];
+
+            //успешный вход через куки
+            //setcookie('user_id', $user['id']);
+            header("Location: /catalog.php");
+
+        } else {
+            $errors['username'] = 'username or password is incorrect';
+        }
+    }
+
+}
+
+
+
+
+
 
 require_once './login_form.php';
